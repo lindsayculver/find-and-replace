abf1823172f39f1bcfbdeab9e5f0f437e581a0e3
Lindsay Culver <leculver@gmail.com>
2015-09-11 09:38:19 -0700

    try something using regExp.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-var replace = function(string, find, replace) {}
+var replaceWord = function(userString, findWord, replace) {
+  return userString.replace(new RegExp(findWord, 'g'), replace);
+};
+
+// $(document).ready(function() {
+//   $('form#findandreplace').submit(function(event) {
+//     var userString = $('input#userString').val();
+//     var findWord = $('input#findWord').val();
+//     var replace = $('input#replace').val();
+//
+//     var result = replaceWord(userString, findWord, replace);
+//
+//     $('#result').text(result);
+//     event.preventDefault();
+//   });
+// });
