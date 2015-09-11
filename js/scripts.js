a30db5946e466f8e1da0eabaf248c7d4011a4607
Lindsay Culver <leculver@gmail.com>
2015-09-11 09:42:58 -0700

    add spec and code for replacing words with js and jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,15 @@ var replaceWord = function(userString, findWord, replace) {
   return userString.replace(new RegExp(findWord, 'g'), replace);
 };
 
-// $(document).ready(function() {
-//   $('form#findandreplace').submit(function(event) {
-//     var userString = $('input#userString').val();
-//     var findWord = $('input#findWord').val();
-//     var replace = $('input#replace').val();
-//
-//     var result = replaceWord(userString, findWord, replace);
-//
-//     $('#result').text(result);
-//     event.preventDefault();
-//   });
-// });
+$(document).ready(function() {
+  $('form#findandreplace').submit(function(event) {
+    var userString = $('input#userString').val();
+    var findWord = $('input#findWord').val();
+    var replace = $('input#replace').val();
+
+    var result = replaceWord(userString, findWord, replace);
+
+    $('#result').text(result);
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe("replace", function() {
   it('recieves user input as a string', function() {
     expect("love").to.equal("love");
   });
+
+  it('finds instances of a findWord and replaces it with a new word in the userString', function() {
+    expect(replaceWord("love is love", "love", "art")).to.equal("art is art");
+  });
 });
